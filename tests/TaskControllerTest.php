82038c8040aff8f33c061c36f2b9bc0c0bf991ab
Eric Johnson <roerjo.personal@gmail.com>
2018-03-25 17:49:32 -0400

    tests passing

--- tests/TaskControllerTest.php
+++ tests/TaskControllerTest.php
@@ -1,16 +1,42 @@
 <?php
 
 use GuzzleHttp\Client;
+use App\Database\Connection;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
 
 class TaskControllerTest extends TestCase
 {
+    use TestCaseTrait;
+
+    private static $pdo = null;
+    private $conn = null;
     protected $client;
+    protected static $id;
+
+    final public function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo === null) {
+                self::$pdo = Connection::make();
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, 'todo');
+        }
+
+        return $this->conn;
+    }
+
+    public function getDataset()
+    {
+        return $this->createArrayDataSet([]);
+    }
 
     public function setUp()
     {
+        parent::setUp();
+
         $this->client = new Client([
-            'base_uri' => 'http://todo.localhost:8000',
+            'base_uri' => 'http://todo.localhost',
         ]);
     }
 
@@ -37,11 +63,20 @@ class TaskControllerTest extends TestCase
 
         //die(var_dump($response->getBody()->getContents()));
         $this->assertEquals(200, $response->getStatusCode());
+
+        $statement = self::$pdo->prepare("
+                SELECT id FROM tasks WHERE title = 'I don\'t wanna'
+        ");
+
+        $statement->execute();
+
+        $results = $statement->fetchAll(PDO::FETCH_CLASS);
+        self::$id = array_pop($results)->id;
     }
 
     public function testItCompletesTask()
     {
-        $response = $this->client->request('POST', '/complete/5');
+        $response = $this->client->request('POST', '/complete/'.self::$id);
 
         //die(var_dump($response->getBody()->getContents()));
         $this->assertEquals(200, $response->getStatusCode());
@@ -49,7 +84,7 @@ class TaskControllerTest extends TestCase
 
     public function testItDeletesTask()
     {
-        $response = $this->client->request('POST', '/delete/5');
+        $response = $this->client->request('POST', '/delete/'.self::$id);
 
         //die(var_dump($response->getBody()->getContents()));
         $this->assertEquals(200, $response->getStatusCode());
